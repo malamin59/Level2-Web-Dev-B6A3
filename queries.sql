87d09d961beaa7521cc38731afe11c596025eb21
learn-git <alaminhossen176466@gmail.com>
2026-01-05 21:51:02 +0600

    Add three practice queries using JOIN, NOT EXISTS, and WHERE clauses

--- queries.sql
+++ queries.sql
@@ -4,3 +4,25 @@ INNER JOIN users ON bookings.user_id = users.user_id
 INNER JOIN vehicles
 ON bookings.vehicle_id = vehicles.vehicle_id
 
+SELECT *
+FROM vehicles v
+WHERE NOT EXISTS (
+    SELECT 1 
+    FROM bookings b
+    WHERE b.vehicle_id = v.vehicle_id
+);
+
+  
+SELECT * FROM vehicles WHERE 
+availability_status  =  'available'
+AND type  = 'car'
+
+
+SELECT 
+    v.vehicle_name,
+    COUNT(b.booking_id) AS total_bookings
+FROM bookings b
+INNER JOIN vehicles v
+    ON b.vehicle_id = v.vehicle_id
+GROUP BY v.vehicle_name
+HAVING COUNT(b.booking_id) > 2;
